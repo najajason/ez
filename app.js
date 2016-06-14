944c4956da30ecd02c88e0284a84fb15efdc8d5b
gapjustin <vuilnisbuilt@gmail.com>
2016-06-14 23:38:41 +0200

    asd

--- app.js
+++ app.js
@@ -1810,7 +1810,6 @@ var MultiplierNumber = React.createClass({
         {className: 'col-xs-12'},
         el.div(
           {
-            className: 'btn-lg btn-success btn-block',
             style: { marginTop: '-15px' }
           },
             'Multiplier: '+totalmultiplier.toFixed(2)
