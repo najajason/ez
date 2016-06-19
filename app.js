2999ffa9f39f73369d6f9e43845b699a13e14a2c
gapjustin <vuilnisbuilt@gmail.com>
2016-06-19 16:09:12 +0200

    asd

--- app.js
+++ app.js
@@ -1856,7 +1856,7 @@ var HotkeyToggle = React.createClass({
             className: 'btn btn-default btn-block btn-lg',
             onClick: this._onClick,
             style: { marginTop: '-15px' },
-		  disabled: (stopped == 0)
+		  disabled: (stopped == 0 && worldStore.state.hotkeysEnabled == false)
           },
           worldStore.state.hotkeysEnabled ?
             'Cashout: '+ ((totalmultiplier-0.01)*betStore.state.wager.num).toFixed(2)+" bits" :
