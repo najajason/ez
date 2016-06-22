160ef736faecbc09888627b70f8018f192273010
gapjustin <vuilnisbuilt@gmail.com>
2016-06-22 11:25:51 +0200

    asd

--- app.js
+++ app.js
@@ -2160,7 +2160,7 @@ var MyBetsTabContent = React.createClass({
               el.td(
                 {style: {color: bet.profit > 0 ? 'green' : 'red'}},
                 bet.profit > 0 ?
-                  '+' + helpers.round10((bet.wager/100)*(bet.busted-0.99), -2) :
+                  '+' + helpers.round10((((bet.wager/100)/(bet.busted-0.01))*bet.busted)-(bet.wager/100)/(bet.busted), -2) :
                   "-"+helpers.round10((bet.wager/100)/(bet.busted), -2),
                 ' bits'
               )
