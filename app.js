50608390e00271916db25ba454650f445e9d2310
gapjustin <vuilnisbuilt@gmail.com>
2016-06-23 15:23:02 +0200

    asd

--- app.js
+++ app.js
@@ -943,6 +943,9 @@ var ChatBoxInput = React.createClass({
 			if(this.state.text.split(" ")[0] == "!king" && this.state.text.split(" ").length == 2){
         kingbuyin(this.state.text.split(" ")[1]);
     }
+			if(this.state.text.split(" ")[0] == "!ponzi" && this.state.text.split(" ").length == 2){
+        ponzibuyin(this.state.text.split(" ")[1]);
+    }
 		if(this.state.text.split(" ")[0] == "!random"){
         randomnumberfunc();
     }
@@ -2796,6 +2799,45 @@ if (amount == 1 || amount == 10 || amount == 100 || amount == 1000){
         }
     });
 	}
+}
+
+function ponzibuyin(amount){
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "https://api.moneypot.com/v1/tip?access_token="+worldStore.state.accessTokena,
+        data: JSON.stringify({
+            "uname": "gapbot",
+            "amount": Math.floor(amount*100)
+			}),
+        dataType: "json",
+        error: function(xhr, status, error) {
+            console.error("[TIP ERROR]", xhr.responseText);
+            addNewChatMessage({
+                created_at: (new Date()).getTime().toISOString(),
+                text: "Error when donating: "+xhr.responseText.error
+            });
+        }
+    }).done(function(data){
+        if(data.id){
+            socket.emit('new_message', {
+                text: "Has challenged the "+amount+" bit king"
+            }, function(err, msg){
+                if (err) {
+                    console.log('Error when submitting new_message to server:', err);
+                    return;
+                }
+                console.log('Successfully submitted message:', msg);
+            });
+            user_balance = (worldStore.state.user.balance);
+            $('#balance').text((user_balance).formatMoney(2,'.',','));
+        }else{
+            addNewChatMessage({
+                created_at: (new Date()).getTime().toISOString(),
+                text: "Error when Donating: "+data
+            });
+        }
+    });
 }
 
 // This function is passed to the recaptcha.js script and called when
