2604ddb1e8d91e950e9c5adbce66d41d689b5f72
gapjustin <vuilnisbuilt@gmail.com>
2016-09-07 19:06:33 +0200

    asd

--- app.js
+++ app.js
@@ -706,7 +706,7 @@ var worldStore = new Store('world', {
     self.state.hotkeysEnabled = !self.state.hotkeysEnabled;
     self.emitter.emit('change', self.state);
 	if (self.state.hotkeysEnabled == true){
-	if (currBetTab == 'BETTING'){
+	if (self.state.currBetTab == 'BETTING'){
 		currentBet = betStore.state.wager.num;
 		} else {
 		}
